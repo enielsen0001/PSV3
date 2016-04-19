e6d4e9586a54e1ec2e8d3e3ac55df529952ab09c
enielsen0001 <enielsen0001@gmail.com>
2016-04-19 16:39:50 -0400

    roughed in nav pulse, parallax and vert tabs

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
+
+	// vertical tabs
    $('#tabs')
     .tabs()
     .addClass('ui-tabs-vertical ui-helper-clearfix');
+
+    //animate menu
+	animateMenu();
+
+    function animateMenu(){
+    
+    var navArray = ["#nav1","#nav2","#nav3","#nav4"];
+    var duration = 2000;
+
+   window.setInterval(function(){
+    navArray.forEach(function(element){
+ 	
+	$( element ).delay(navArray.indexOf(element)*duration).animate({
+          color: "#36e300",
+          width: 500
+        }, 3000 );
+	$( element ).animate({
+          color: "#000",
+          width: 500
+        }, 3000 );
+
+ 		});
+   },11000);
+   
+   
+   
+}//and func anim menu
 });
